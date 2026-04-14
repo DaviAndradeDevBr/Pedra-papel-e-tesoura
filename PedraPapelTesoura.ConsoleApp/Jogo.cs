a9ad011d68b3ff089ecb622015c26b7fccc24192
Davi Andrade <contatodaviandrade20@gmail.com>
2026-04-14 19:56:39 -0300

    Implementado classe de estilização do sistema

--- PedraPapelTesoura.ConsoleApp/Jogo.cs
+++ PedraPapelTesoura.ConsoleApp/Jogo.cs
@@ -11,75 +11,101 @@ class Jogo
             if (escolhaJogador == ESCOLHA_PEDRA)
             {
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("Pedra vs Pedra");
+                Console.ResetColor();
             }
             else if (escolhaJogador == ESCOLHA_PAPEL)
             {
                 Console.WriteLine("------------------------------------");
+                 Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("Papel vs Papel");
+                Console.ResetColor();
             }
             else if (escolhaJogador == ESCOLHA_TESOURA)
             {
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("Tesoura vs Tesoura");
+                Console.ResetColor();
             }
 
             Console.WriteLine("------------------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             Console.WriteLine("Empate!");
+            Console.ResetColor();
             Console.WriteLine("------------------------------------");
         }
         else if (escolhaJogador == ESCOLHA_PEDRA)
         {
             Console.WriteLine("------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("Pedra vs ");
+            Console.ResetColor();
 
             if (escolhaComputador == ESCOLHA_PAPEL)
             {
                 Console.WriteLine("Papel");
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("O computador venceu.");
+                Console.ResetColor();
             }
             else if (escolhaComputador == ESCOLHA_TESOURA)
             {
                 Console.WriteLine("Tesoura");
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("Você venceu.");
+                Console.ResetColor();
             }
         }
         else if (escolhaJogador == ESCOLHA_PAPEL)
         {
             Console.WriteLine("------------------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.Write("Papel vs ");
+            Console.ResetColor();
 
             if (escolhaComputador == ESCOLHA_TESOURA)
             {
                 Console.WriteLine("Tesoura");
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("O computador venceu.");
+                Console.ResetColor();
             }
             else if (escolhaComputador == ESCOLHA_PEDRA)
             {
                 Console.WriteLine("Pedra");
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("Você venceu.");
+                Console.ResetColor();
             }
         }
         else if (escolhaJogador == ESCOLHA_TESOURA)
         {
             Console.WriteLine("------------------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
             Console.Write("Tesoura vs ");
+            Console.ResetColor();
 
             if (escolhaComputador == ESCOLHA_PEDRA)
             {
                 Console.WriteLine("Pedra");
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("O computador venceu.");
+                Console.ResetColor();
             }
             else if (escolhaComputador == ESCOLHA_PAPEL)
             {
                 Console.WriteLine("Papel");
                 Console.WriteLine("------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.WriteLine("Você venceu.");
+                Console.ResetColor();
             }
         }
     }
--- PedraPapelTesoura.ConsoleApp/Program.cs
+++ PedraPapelTesoura.ConsoleApp/Program.cs
@@ -1,8 +1,5 @@
 ﻿class Program
 {
-
-
-
     static void Main(string[] args)
     {
         while (true)
@@ -15,8 +12,10 @@
 
             Jogo.CompararEscolhas(escolhaJogador, escolhaComputador);
 
-            if (JogadorDesejaContinuar())
+            if (!JogadorDesejaContinuar())
+            {
                 break;
+            }
 
 
         }
@@ -25,14 +24,28 @@
     static void ExibirTitulo()
     {
         Console.Clear();
+
+        Console.BackgroundColor = ConsoleColor.Gray;
+
         Console.WriteLine("------------------------------------");
-        Console.WriteLine("Pedra, papel e tesoura");
-        Console.WriteLine("------------------------------------");
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.Write("Pedra,");
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.Write(" papel");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(" e");
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write(" tesoura");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("\n------------------------------------");
+
+        Console.BackgroundColor = ConsoleColor.Black;
 
     }
 
     static bool JogadorDesejaContinuar()
     {
+        Console.WriteLine("------------------------------------");
         Console.Write("Deseja continuar? (S/N): ");
 
         if (Console.ReadLine()?.ToUpper() != "S")
